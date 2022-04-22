f0ce29c0e01a467f58f029dc2391533198c886c9
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-22 16:21:38 +0530

    Add card list component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Banner from '../components/banner';
+import Card from '../components/card';
 import styles from '../styles/Home.module.css';
+import coffeeStores from '../data/coffee-stores.json';
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {};
@@ -21,6 +23,17 @@ export default function Home() {
         />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
+        <div className={styles.cardLayout}>
+          {coffeeStores.map((coffeeStore) => (
+            <Card
+              key={coffeeStore.id}
+              className={styles.card}
+              name={coffeeStore.name}
+              imgUrl={coffeeStore.imgUrl}
+              href={`/coffee-store/${coffeeStore.id}`}
+            />
+          ))}
         </div>
       </main>
     </div>
